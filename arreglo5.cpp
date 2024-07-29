69e93723e079f1e07dfc42f9e190e26f475ca3e7
afloresh1 <afloresh@unjbg.edu.pe>
2024-07-29 14:33:09 -0500

    Se modificó el ejercicio para agregar el tercer arreglo que pide el ejercicio, debido a que no se pudo agregar como es debido

--- arreglo5.cpp
+++ arreglo5.cpp
@@ -1,4 +1,5 @@
 //Ejercicio propuesto 05//
+//Ejercicio propuesto 05//
 //Alumnos comunes de dos asignaturas
 //Alison Khiara Flores Hallasi//
 #include<iostream>
@@ -6,13 +7,14 @@
 using namespace std;
 int main(){
 	int nfp, npg;
-	string fp[999];
-	string pg[999];
+	string fp[9999];
+	string pg[9999];
+	string alc[1000];
 	cout<<"Ingrese la cantidad de estudiantes de Fundamentos de programacion: ";
 	cin>>nfp;
 	cout<<"Ingrese la cantidad de estudiantes de Programacion Grafica: ";
 	cin>>npg;
-	for (int i=1; i<=nfp;i++){
+	for (int i=1; i<= nfp;i++){
 		cout<<"Ingrese el nombre del estudiante en Fundamentos: ";
 		cin>>fp[i];
 	}
@@ -24,8 +26,10 @@ int main(){
 	for (int j=1; j<=nfp;j++){
 		for(int k=1;k<=npg;k++){
             if(fp[j]==pg[k]){
-            cout<<fp[j]<<endl;
+            	alc[j]=fp[j];
+            cout<<alc[j]<<endl;
         	}
         }
 	}
+	return 0;
 }
